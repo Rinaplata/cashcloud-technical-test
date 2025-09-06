f3e3206873b0861e9b5bec634977cd2434ee9867
Rina Plata <rina.plata@syspotec.com>
2025-09-06 15:16:44 -0500

    FEAT: create reusable paymentSummaryCard

--- cashcloud-app/src/app/app.component.ts
+++ cashcloud-app/src/app/app.component.ts
@@ -2,15 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { StatusBadgeComponent } from './shared/status-badge/status-badge.component';
 import { PaymentStatus } from './core/enums/paymentStatus';
+import { PaymentSummaryCardComponent } from './shared/payment-summary-card/payment-summary-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, StatusBadgeComponent],
+  imports: [CommonModule, StatusBadgeComponent, PaymentSummaryCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
   PaymentStatus = PaymentStatus;
+  title='No. of Payments:'
 }
---
+++ cashcloud-app/src/app/shared/payment-summary-card/payment-summary-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input} from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-payment-summary-card',
+  standalone: true,
+  imports: [CardModule],
+  templateUrl: './payment-summary-card.component.html',
+  styleUrl: './payment-summary-card.component.css'
+})
+export class PaymentSummaryCardComponent {
+  @Input() title!: string;
+  @Input() totalPayments!: number;
+  @Input() checks!: number;
+  @Input() ach!: number;
+  @Input() digitalChecks!: number;
+
+
+}
